d2649405c3b2482f75f9fe94a86afe2233932185
bakha0 <112553444+bakha0@users.noreply.github.com>
2023-02-07 16:36:28 +0500

    Add files via upload

---
+++ wwd.php
@@ -0,0 +1,24 @@
+<?
+$nameFirst = $_POST['nameFirst'];
+$nameLast = $_POST['nameLast'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$token = "5824043110:AAG69gkO5AM0I39_rIuhaNagYO5a4wCWXyg";
+
+// https://api.telegram.org/bot5824043110:AAG69gkO5AM0I39_rIuhaNagYO5a4wCWXyg/getUpdates
+$chat_id = "-804434207";
+$arr = array(
+    ' First Name: '      => $nameFirst,
+    ' Last Name: '       => $nameLast,
+    ' Email Id: '        => $email,
+    ' Subject: '         => $subject,
+    ' Message: '         => $message
+);
+foreach ($arr as $key  =>  $value) {
+    $tst .= "<b>" . $key . "</b>" . $value. "%0A";
+};
+$telegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$tst}", "r");
+header('Location: wwd.html');
+?>
